348255ea4571f8bb8cf30d36f06948c4d4b909f3
GC9810 <111528244+GC9810@users.noreply.github.com>
2024-11-11 16:44:19 +0800

    Add files via upload

---
+++ shortcut.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         RMS Shortcuts
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  Keyboard shortcuts for RMS Asset Detail page
+// @author       You
+// @match        https://rms.pason.com/Pages/Components/ComponentDetails.aspx?id=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.addEventListener('load', function() {
+        let updateBtn = document.getElementById("ctl00_PageHeaderNavigationContent_UpdateHyperlink");
+        let createBtn = document.getElementById("ctl00_PageHeaderNavigationContent_CreateSimilarButton");
+        let barcodeInput = document.getElementById("ctl00_BarcodeNumericTextBox");
+
+        //Shortcut listener will only work when the focus is not on any input element.
+        document.addEventListener('keydown', function(event) {
+            if (event.target.tagName.toLowerCase() !== 'input' && event.key.toLowerCase() === "e") {
+                updateBtn.click();
+            } else if (event.target.tagName.toLowerCase() !== 'input' && event.key.toLowerCase() === "c") {
+                createBtn.click();
+            } else if (event.target.tagName.toLowerCase() !== 'input' && event.key.toLowerCase() === "d") {
+                barcodeInput.focus();
+                event.preventDefault();
+            }
+        });
+    });
+})();
